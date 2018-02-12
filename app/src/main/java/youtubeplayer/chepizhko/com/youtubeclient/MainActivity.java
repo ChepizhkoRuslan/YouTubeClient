2c46a504e176926e5ae1db6c8b0395a22d79aa5c
Vladyslav Boiko <vladtj78.developer@gmail.com>
2018-02-12 21:47:14 +0200

    init commit

---
+++ app/src/main/java/youtubeplayer/chepizhko/com/youtubeclient/MainActivity.java
@@ -0,0 +1,13 @@
+package youtubeplayer.chepizhko.com.youtubeclient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
